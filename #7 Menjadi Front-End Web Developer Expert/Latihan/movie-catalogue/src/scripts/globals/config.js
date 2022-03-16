d1ac64f24072af35d9c0e669fa99dde318f4b255
milyasarmans <milyasarmans13@gmail.com>
2022-03-17 00:52:11 +0700

    movie-catalogue

---
+++ #7 Menjadi Front-End Web Developer Expert/Latihan/movie-catalogue/src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    KEY: '087c73784696cf53db083e374cebf9e9',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+  };
+   
+  export default CONFIG;
